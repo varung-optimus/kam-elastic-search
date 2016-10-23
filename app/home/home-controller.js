018b2665152c8444758ddf6a0a8bbf68ead639e6
Avinash Singh <avinash4507@gmail.com>
2016-10-23 21:41:50 +0530

    added sidenav

--- app/home/home-controller.js
+++ app/home/home-controller.js
@@ -10,10 +10,35 @@
    */
   angular
     .module('home')
-    .controller('HomeCtrl', HomeCtrl);
+    .controller('HomeCtrl', HomeCtrl)
+    .controller('LeftCtrl', LeftCtrl);
 
-  function HomeCtrl() {
+  function HomeCtrl($timeout, $mdSidenav, $log) {
     var vm = this;
     vm.ctrlName = 'HomeCtrl';
+    vm.toggleLeft = buildToggler('left');
+    vm.isOpenLeft = function () {
+      return $mdSidenav('left').isOpen();
+    };
+    function buildToggler(navID) {
+      return function () {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug('toggle ' + navID + ' is done');
+          });
+      };
+    }
+  }
+  function LeftCtrl($timeout, $mdSidenav, $log) {
+    var vm = this;
+    vm.close = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug('close Left is done');
+        });
+    };
   }
 }());
