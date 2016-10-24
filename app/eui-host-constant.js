4c3f18758cbe0c7af5c67e690b51de6f07bdfebd
Varun Goel <varun.goel@optimusinfo.com>
2016-10-25 00:29:30 +0530

    Removed test file

--- app/eui-host-constant.js
+++ app/eui-host-constant.js
@@ -10,5 +10,6 @@
    */
   angular
     .module('kamElasticSearch')
-    .constant('euiHost', 'http://41.106.2.2:9200');
+    .constant('euiHost', 'http://41.106.2.2:9200')
+    .constant('proxyServer', 'http://52.51.115.98/');
 }());
