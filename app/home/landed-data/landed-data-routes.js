299684112d71c2ecde8044ef88ce9c4d0e8b1b8a
Varun Goel <varun.goel@optimusinfo.com>
2016-10-25 07:53:00 +0530

    Fixed the tab reload issue

--- app/home/landed-data/landed-data-routes.js
+++ app/home/landed-data/landed-data-routes.js
@@ -8,7 +8,7 @@
   function config($stateProvider) {
     $stateProvider
       .state('home.landedData', {
-        url: '/landed-data',
+        // url: '/landed-data',
         templateUrl: 'home/landed-data/landed-data.tpl.html',
         controller: 'LandedDataCtrl',
         controllerAs: 'landedData'
