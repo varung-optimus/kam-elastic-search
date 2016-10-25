78d8275655fed876da1afeec16f677d05690fc20
Varun Goel <varun.goel@optimusinfo.com>
2016-10-25 07:46:46 +0530

    Loaded tabs from UI Router

--- app/home/landed-data/landed-data-routes.js
+++ app/home/landed-data/landed-data-routes.js
@@ -12,6 +12,12 @@
         templateUrl: 'home/landed-data/landed-data.tpl.html',
         controller: 'LandedDataCtrl',
         controllerAs: 'landedData'
+      })
+      .state('home.landedData.hdfsTab', {
+        templateUrl: 'home/landed-data/partials/_hdfs-tab.html'
+      })
+      .state('home.landedData.hiveTab', {
+        templateUrl: 'home/landed-data/partials/_hive-tab.html'
       });
   }
 }());
